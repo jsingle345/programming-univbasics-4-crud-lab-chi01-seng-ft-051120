9a8a3b39498103e900fb4d4b78e10404e0366186
Joshua Singleton <jsingle345@gmail.com>
2020-04-29 22:23:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   fruit = ["apple", "pear", "orange", "grapes"]
-  fruit << "plum"
+  fruit.push("plum")
 end
 
 def add_element_to_start_of_array(array, element)
