baf323476259fb8a903ef3f5cc3b7ea41db6165c
Joshua Singleton <jsingle345@gmail.com>
2020-04-29 22:27:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["apple", "pear", "orange", "grapes"]
-  array.pop("grapes")
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
